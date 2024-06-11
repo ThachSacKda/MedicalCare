da10be8ecf23adfced1b6e9da418f71b1a90c845
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-06-11 18:54:25 +0700

    edit user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -25,11 +25,31 @@ let postCRUD = async (req, res)=>{
 
 let displayGetCRUD = async (req,res) => {
     let data = await CRUDService.getAllUser();
-    console.log('-------------------')
-    console.log(data)
-    console.log('-------------------')
     return res.render('displayCRUD.ejs', {
         dataTable: data
+    })
+}
+
+let getEditCRUD = async(req, res) => {
+    let userId = req.query.id;
+    console.log(userId)
+    if(userId){
+        let userData = await CRUDService.getUserInforById(userId);
+        //check user data not found
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else{
+        return res.send('User not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
     })
 }
 
@@ -39,4 +59,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
