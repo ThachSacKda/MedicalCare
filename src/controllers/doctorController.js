7d513ba7873c0ae419f14d2b924e8de4b40860aa
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-08-01 11:21:16 +0700

    show doctor in homepage

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,20 @@
+import doctorService from "../services/doctorService";
+
+let getDoctorHome = async (req, res) => {
+    let limit = req.query.limit;
+    if (!limit) limit = 10;
+    try {
+        let response = await doctorService.getDoctorHome(+limit);
+        return res.status(200).json(response);
+    } catch (e) {
+        console.error('Error in getDoctorHome controller:', e); // Ghi lại chi tiết lỗi
+        return res.status(200).json({ // Sửa lại mã lỗi HTTP
+            errCode: -1,
+            message: 'Error from service...'
+        });
+    }
+};
+
+module.exports = {
+    getDoctorHome: getDoctorHome
+};
